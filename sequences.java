8e993b7172016bda0b204f4bd082e37a28ee50ff
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-05-22 15:36:47 -0400

    added operation

--- sequences.java
+++ sequences.java
@@ -17,8 +17,8 @@ class sequences {
         // loop
         while (true) {
             System.out.println("\nWhich equation do you want to use?");
-            System.out.println("1. tn = a + (n-1)d \n2. tn = ar^(n - 1) \n3. Sn = n/2(2a + (n - 1)d) \n4. Sn = a(r^n - 1)/(r-1) \n5. Sn = n/2(a + tn)" );
-            System.out.println("Press 6 to exit");
+            System.out.println("1. tn = a + (n-1)d \n2. tn = ar^(n - 1) \n3. Sn = n/2(2a + (n - 1)d) \n4. Sn = a(r^n - 1)/(r-1) \n5. Sn = n/2(a + tn) \n6. Solve for Sum (Sn)" );
+            System.out.println("Press 7 to exit");
             System.out.print("   >  ");
             selection = In.getInt();
 
@@ -33,6 +33,8 @@ class sequences {
             } else if (selection == 5){
             arithmeticSeriesWithTN();
             } else if (selection == 6){
+                solveSn();
+            } else if (selection == 7){
                 System.out.println("\nBye Bye");
                 break;
             }
@@ -147,6 +149,29 @@ class sequences {
 
         answer = (int) (x * y);
  
+        // print answer
+        System.out.println("\nYour answer is: " + answer);
+    }
+
+    public static void solveSn() {
+        System.out.println("\nSolve for Sum (Sn)");
+
+        // gather variables
+        System.out.println("\nWhat is your a variable?");
+        a = In.getDouble();
+        System.out.println("\nWhat is your n variable?");
+        n = In.getDouble();
+        System.out.println("\nWhat is your d variable?");
+        d = In.getInt();
+        System.out.println("\nWhat is your sum (Sn)?");
+        Sn = In.getInt();
+
+        // do math 
+        double x = (int) (n - 1);
+        double y = (int) a * 2;
+
+        answer = (int) (n * (y + (x * d))) / 2;
+
         // print answer
         System.out.println("\nYour answer is: " + answer);
     }
